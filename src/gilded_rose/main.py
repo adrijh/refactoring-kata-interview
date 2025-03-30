6234e4f65f4388995be00f8a40dc1d6f8c332f98
Adrian Jimenez <ajimenezh.94@gmail.com>
2025-03-30 13:19:19 +0200

    Extract update item logic

--- src/gilded_rose/main.py
+++ src/gilded_rose/main.py
@@ -4,44 +4,47 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-    def update_quality(self):
+    def update_inventory(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Ticket to Lucas The Bard concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        # Handle standard item quality
-                        item.quality = item.quality - 1
+            self.update_item(item)
+
+    def update_item(self, item):
+        if item.name != "Aged Brie" and item.name != "Ticket to Lucas The Bard concert":
+            if item.quality > 0:
+                if item.name != "Sulfuras, Hand of Ragnaros":
+                    # Handle standard item quality
+                    item.quality = item.quality - 1
+        else:
+            if item.quality < 50:
+                # Handle quality increasing item
+                item.quality = item.quality + 1
+                if item.name == "Ticket to Lucas The Bard concert":
+                    if item.sell_in < 11:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+                    if item.sell_in < 6:
+                        if item.quality < 50:
+                            item.quality = item.quality + 1
+
+        if item.name != "Sulfuras, Hand of Ragnaros":
+            # Handle standard item sell_in
+            item.sell_in = item.sell_in - 1
+
+        if item.sell_in < 0:
+            # Handle expired items
+            if item.name != "Aged Brie":
+                if item.name != "Ticket to Lucas The Bard concert":
+                    if item.quality > 0:
+                        if item.name != "Sulfuras, Hand of Ragnaros":
+                            # Standard item extra quality loss
+                            item.quality = item.quality - 1
+                else:
+                    # Ticket goes to 0 if expired
+                    item.quality = item.quality - item.quality
             else:
                 if item.quality < 50:
-                    # Handle quality increasing item
+                    # Aged Brie extra quality gain
                     item.quality = item.quality + 1
-                    if item.name == "Ticket to Lucas The Bard concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                # Handle standard item sell_in
-                item.sell_in = item.sell_in - 1
-
-            if item.sell_in < 0:
-                # Handle expired items
-                if item.name != "Aged Brie":
-                    if item.name != "Ticket to Lucas The Bard concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                # Standard item extra quality loss
-                                item.quality = item.quality - 1
-                    else:
-                        # Ticket goes to 0 if expired
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        # Aged Brie extra quality gain
-                        item.quality = item.quality + 1
 
 
 @dataclass
