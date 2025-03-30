ebcfd9f1ae900dad81ad2bc583371242925af88b
Adrian Jimenez <ajimenezh.94@gmail.com>
2025-03-30 12:57:01 +0200

    tests improvements

--- tests/unit/test_gilded_rose.py
+++ tests/unit/test_gilded_rose.py
@@ -1,12 +1,12 @@
 from src.gilded_rose.main import Item, GildedRose
 
 def test_standard_item():
-    items = [
+    got = perform_items_upgrade([
         Item(name="+5 Dexterity Vest", sell_in=10, quality=9),
         Item(name="Elixir of the Mongoose", sell_in=1, quality=12),
         Item(name="Enigmatic Staff", sell_in=0, quality=16),
         Item(name="Necklace of Ice", sell_in=-12, quality=0),
-    ]
+    ])
 
     want = [
         Item(name="+5 Dexterity Vest", sell_in=9, quality=8),
@@ -15,21 +15,15 @@ def test_standard_item():
         Item(name="Necklace of Ice", sell_in=-13, quality=0),
     ]
 
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    got = gilded_rose.items
-
-    # print(f"{got=}")
-    # print(f"{want=}")
-
     assert got == want
 
+
 def test_aged_brie():
-    items = [
+    got = perform_items_upgrade([
         Item(name="Aged Brie", sell_in=2, quality=0),
         Item(name="Aged Brie", sell_in=-3, quality=5),
         Item(name="Aged Brie", sell_in=0, quality=50),
-    ]
+    ])
 
     want = [
         Item(name="Aged Brie", sell_in=1, quality=1),
@@ -37,43 +31,31 @@ def test_aged_brie():
         Item(name="Aged Brie", sell_in=-1, quality=50),
     ]
 
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    got = gilded_rose.items
-
-    # print(f"{got=}")
-    # print(f"{want=}")
-
     assert got == want
 
+
 def test_sulfuras():
-    items = [
+    got = perform_items_upgrade([
         Item(name="Sulfuras, Hand of Ragnaros", sell_in=2, quality=80),
         Item(name="Sulfuras, Hand of Ragnaros", sell_in=-20, quality=80),
-    ]
+    ])
 
     want = [
         Item(name="Sulfuras, Hand of Ragnaros", sell_in=2, quality=80),
         Item(name="Sulfuras, Hand of Ragnaros", sell_in=-20, quality=80),
     ]
 
-    gilded_rose = GildedRose(items)
-    gilded_rose.update_quality()
-    got = gilded_rose.items
-
-    # print(f"{got=}")
-    # print(f"{want=}")
-
     assert got == want
 
+
 def test_bard_ticket():
-    items = [
+    got = perform_items_upgrade([
         Item(name="Ticket to Lucas The Bard concert", sell_in=20, quality=10),
         Item(name="Ticket to Lucas The Bard concert", sell_in=10, quality=10),
         Item(name="Ticket to Lucas The Bard concert", sell_in=5, quality=10),
         Item(name="Ticket to Lucas The Bard concert", sell_in=0, quality=0),
         Item(name="Ticket to Lucas The Bard concert", sell_in=-1, quality=10),
-    ]
+    ])
 
     want = [
         Item(name="Ticket to Lucas The Bard concert", sell_in=19, quality=11),
@@ -83,11 +65,10 @@ def test_bard_ticket():
         Item(name="Ticket to Lucas The Bard concert", sell_in=-2, quality=0),
     ]
 
+    assert got == want
+
+
+def perform_items_upgrade(items: list[Item]) -> list[Item]:
     gilded_rose = GildedRose(items)
     gilded_rose.update_quality()
-    got = gilded_rose.items
-
-    print(f"{got=}")
-    print(f"{want=}")
-
-    assert got == want
+    return gilded_rose.items
